367d39895cb52e4a8b001e0b511dd861bcdc5577
JarodLge <137367566+JarodLgeOff@users.noreply.github.com>
2025-10-21 20:26:05 +0000

    feat: add script to print the alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char c = 97;
+
+	while (c <= 123)
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
