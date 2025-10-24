771a881b706477a1415e43156440d85a792270fd
JarodLge <137367566+JarodLgeOff@users.noreply.github.com>
2025-10-24 17:29:59 +0000

    feat: implement _abs function to return absolute value and add main function

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _abs - Entry point
+* @c: c is integer
+* Return: absolute of c
+*/
+
+int _abs(int c)
+{
+	if (c < 0)
+	{
+		return (-c);
+	}
+	else
+	{
+		return (c);
+	}
+}
