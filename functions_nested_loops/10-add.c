362ff4dec06b7d1a52fd3e31ceeab5c1cce4bbc2
JarodLge <137367566+JarodLgeOff@users.noreply.github.com>
2025-10-24 18:51:16 +0000

    feat: implement add function to return the sum of two integers and  main function

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* add - Entry point
+*
+* @a: first int
+* @b: second int
+*
+* Return: The result of the add of two integer
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
