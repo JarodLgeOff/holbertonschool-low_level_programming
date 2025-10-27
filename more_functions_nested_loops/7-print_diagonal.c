85c902d7d355011caf77d4477f63a6520cdf0674
JarodLge <pro.jarodlge@gmail.com>
2025-10-27 21:19:44 +0100

    refactor: optimize print_diagonal function for clarity and performance

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -8,23 +8,17 @@
  */
 void print_diagonal(int n)
 {
-	int i, j;
-
 	if (n > 0)
 	{
-		for (i = 0 ; i < n ; i++)
+		int i, j;
+
+		for (i = 0; i < n; i++)
 		{
-			for (j = 0 ; j < n ; j++)
+			for (j = 0; j < i; j++)
 			{
-				if (i == j)
-				{
-					_putchar('\\');
-				}
-				else
-				{
-					_putchar(' ');
-				}
+				_putchar(' ');
 			}
+			_putchar('\\');
 			_putchar('\n');
 		}
 	}
