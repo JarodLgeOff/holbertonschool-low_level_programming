6990e0aab7de5a737a0b83fea87f703d8a2eba9b
JarodLge <pro.jarodlge@gmail.com>
2025-11-03 13:31:54 +0100

    feat: implement _strspn function to calculate the length of a prefix substring and update header file

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: string to search
+ * @accept: string containing the characters to match
+ *
+ * Return: number of bytes in the initial segment of s
+ * which consist only of bytes from accept
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int count = 0;
+	char *a;
+
+	while (*s)
+	{
+		a = accept;
+		while (*a)
+		{
+			if (*s == *a)
+			{
+				count++;
+				break;
+			}
+			a++;
+		}
+		if (*a == '\0')
+			break;
+		s++;
+	}
+	return (count);
+}
