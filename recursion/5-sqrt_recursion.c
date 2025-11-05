c96b27220110818e4d13e2f29c9602e6675d1f25
JarodLge <pro.jarodlge@gmail.com>
2025-11-05 11:06:23 +0100

    fix: improve readability by adding braces in _sqrt_helper and _sqrt_recursion functions

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -9,9 +9,13 @@
 int _sqrt_helper(int n, int i)
 {
 	if (i * i == n)
+	{
 		return (i);
+	}
 	if (i * i > n)
+	{
 		return (-1);
+	}
 	return (_sqrt_helper(n, i + 1));
 }
 /**
@@ -23,6 +27,8 @@ int _sqrt_helper(int n, int i)
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
+	{
 		return (-1);
+	}
 	return (_sqrt_helper(n, 0));
 }
