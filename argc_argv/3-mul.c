6206f1fe45c02f06488c7558c996a23a837e93c9
JarodLge <contact.jarodlge@gmail.com>
2025-11-07 15:53:21 +0100

    feat: add program to multiply two numbers from command line arguments

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints the name of the program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		int a, b, c;
+
+		a = atoi(argv[1][0] == '-' ? argv[1] + 1 : argv[1]);
+		if (argv[1][0] == '-')
+			a = -a;
+		b = atoi(argv[2][0] == '-' ? argv[2] + 1 : argv[2]);
+		if (argv[2][0] == '-')
+			b = -b;
+		c = a * b;
+		printf("%d\n", c);
+		return (0);
+	}	
+}
