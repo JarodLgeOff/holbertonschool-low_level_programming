3ad5db343401b97a0c540e24a13606656a0017ff
JarodLge <contact.jarodlge@gmail.com>
2025-11-10 17:54:26 +0100

    fix: reorder NULL check for memory allocation in create_array function

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -21,14 +21,14 @@ char *create_array(unsigned int size, char c)
 
 	a = (char *)malloc(size * sizeof(char));
 
-	for (i = 0; i < size; i++)
-	{
-		a[i] = c;
-	}
-
 	if (a == NULL)
 	{
 		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		a[i] = c;
 	}
 
 	return (a);
