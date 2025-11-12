c8661f362ab4048014f6df0eda2268e715c3526e
JarodLge <contact.jarodlge@gmail.com>
2025-11-12 22:50:59 +0100

    feat: implement _calloc function for memory allocation and add main program for testing

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _calloc - allocates memory for an array, using malloc
+ * @nmemb: number of elements
+ * @size: size of each element
+ *
+ * Return: pointer to allocated memory, or NULL on failure
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+	unsigned int total_size;
+	unsigned int i;
+	char *char_ptr;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	total_size = nmemb * size;
+
+	ptr = malloc(total_size);
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	char_ptr = (char *)ptr;
+
+	for (i = 0; i < total_size; i++)
+	{
+		char_ptr[i] = 0;
+	}
+
+	return (ptr);
+}
