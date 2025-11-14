b68b4f0d30630812da9e7ea2f366a015da88a3c6
JarodLge <contact.jarodlge@gmail.com>
2025-11-14 22:21:47 +0100

    feat: implement free_dog function and main program for testing

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - Entry point
+ * @d: ptr
+ * Return: Always 0 (Success)
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
