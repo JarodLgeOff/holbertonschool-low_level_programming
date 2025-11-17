8e219bfbf0ca27281aafba4f41a9f5fad3ae7d51
JarodLge <contact.jarodlge@gmail.com>
2025-11-17 20:37:17 +0100

    style: add newline at the beginning of int_index function

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -10,6 +10,7 @@
  * Return: the index of the first element for which the cmp function does not
  * return 0, or -1 if no element matches or size <= 0
  */
+
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
