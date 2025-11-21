153ed94e308d32a817c5fd4e8c2479384cc2a62c
JarodLge <contact.jarodlge@gmail.com>
2025-11-21 20:09:25 +0100

    feat: implement variadic functions for summing parameters and add main test program

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - return the sum of parameters
+ * @n: number of parameters
+ *
+ * Return: 0
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	int sum = 0;
+	unsigned int i;
+
+	va_list args;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(args, int);
+
+	va_end(args);
+
+	return (sum);
+}
