595b840363631110aa56fef2ac9c330287a5b84d
JarodLge <contact.jarodlge@gmail.com>
2025-12-03 00:55:11 +0100

    feat: implement free_list function to free a singly linked list

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_list - frees a singly linked list
+ * @head: pointer to the head of the list
+ */
+void free_list(list_t *head)
+{
+	list_t *current;
+
+	while (head)
+	{
+		current = head;
+		head = head->next;
+		free(current->str);
+		free(current);
+	}
+}
