7cc79bb0428e830f28c79845e321e685c7b55069
JarodLge <contact.jarodlge@gmail.com>
2025-12-03 19:26:49 +0100

    feat: add documentation for add_dnodeint function

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -1,5 +1,13 @@
 #include "lists.h"
 
+/**
+ * add_dnodeint - adds a new node at the beginning of a dlistint_t list
+ * @head: pointer to the head of the list
+ * @n: integer to be added to the new node
+ *
+ * Return: address of the new node, or NULL if it failed
+ */
+
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node;
