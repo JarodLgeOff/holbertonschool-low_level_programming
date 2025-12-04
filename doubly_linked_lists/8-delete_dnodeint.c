1dc282954b563bcaf4f0973986e47bb242edc5cc
JarodLge <pro.jarodlge@gmail.com>
2025-12-04 20:56:47 +0100

    fix: correct return values in delete_dnodeint_at_index function to indicate failure

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -14,14 +14,14 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	unsigned int i;
 
 	if (head == NULL || *head == NULL)
-		return (0);
+		return (-1);
 
 	current = *head;
 	for (i = 0; current != NULL && i < index; i++)
 		current = current->next;
 
 	if (current == NULL)
-		return (0);
+		return (-1);
 
 	if (current->prev != NULL)
 		current->prev->next = current->next;
