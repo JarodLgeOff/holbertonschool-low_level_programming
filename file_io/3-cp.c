a60f853bc61cb9d60b9498dd5b1044d632433319
JarodLge <contact.jarodlge@gmail.com>
2025-12-05 18:53:49 +0100

    fix: enhance error handling in main function for file copy operations

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -93,6 +93,15 @@ int error_close(int fd_back, int fd_go)
 	}
 	return (err_close);
 }
+
+/**
+ * main - Copies the content of a file to another file.
+ * @ac: Argument count.
+ * @av: Argument vector.
+ *
+ * Return: 0 on success, or exit with error code on failure.
+ */
+
 int main(int ac, char *av[])
 {
 	int fd_from, fd_to, c1, c2;
@@ -100,37 +109,53 @@ int main(int ac, char *av[])
 	char buffer[1024];
 
 	if (ac != 3)
+	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
+	}
 
 	fd_from = open(av[1], O_RDONLY);
 	if (fd_from == -1)
+	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
+	}
 
 	fd_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_to == -1)
+	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 		exit(99);
+	}
 
 	while ((nread = read(fd_from, buffer, 1024)) > 0)
+	{
 		if (write(fd_to, buffer, nread) != nread)
+		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 			exit(99);
+		}
+	}
 
 	if (nread == -1)
+	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
+	}
 
 	c1 = close(fd_from);
 	if (c1 == -1)
+	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
 		exit(100);
+	}
 
 	c2 = close(fd_to);
 	if (c2 == -1)
+	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
 		exit(100);
+	}
 
 	return (0);
 }
